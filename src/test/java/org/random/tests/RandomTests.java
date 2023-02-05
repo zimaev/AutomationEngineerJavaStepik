653863b2c0dc79a624dc08d2780aed992809f710
Anton Zimaev <zimaew@gmail.com>
2023-02-05 15:27:20 +0300

    cilick nutton random

--- src/test/java/org/random/tests/RandomTests.java
+++ src/test/java/org/random/tests/RandomTests.java
@@ -1,11 +1,50 @@
 package org.random.tests;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
 
 public class RandomTests {
 
+    static WebDriver driver;
+    static WebDriverWait wait;
+
+    @BeforeEach
+    void setUp() {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/admin/Downloads/chromedriver_mac64/chromedriver");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+    }
+
     @Test
-    void randomNumber(){
+    void randomNumber() {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/admin/Downloads/chromedriver_mac64/chromedriver");
+
+
+        driver.get("https://www.random.org/");
+        wait.until(
+                ExpectedConditions.frameToBeAvailableAndSwitchToIt(
+                        By.cssSelector("#homepage-generator iframe")));
+
+        driver.findElement(By.cssSelector("[value=\"Generate\"]")).click();
+
+    }
+
+    @AfterAll
+    static void tearDown() {
+        driver.quit();
 
     }
 }
